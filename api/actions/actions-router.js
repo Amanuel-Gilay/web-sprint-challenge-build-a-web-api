6c34577ea90199e3d5b6dd8b805050c582ad9647
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-06 14:07:44 -0700

    sprint

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,8 @@
 // Write your "actions" router here!
+const express = require('express');
+
+
+const action = require('./actions-model')
+const project = require('../actions/actions-model')
+
+module.exports = server;
