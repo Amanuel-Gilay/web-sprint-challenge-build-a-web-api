5887623ccdb3dc0f3dc3ad7b2ae3ae3bf82ef9a0
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-06 17:22:07 -0700

    finished sprint

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const server = require('./api/server.js')
+
+const PORT = 5000
+
+ server.listen(PORT, () => {
+    console.log('listen on ', PORT);
+   });
